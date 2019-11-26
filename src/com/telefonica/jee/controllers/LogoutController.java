a61c631f6fcb7b8e5609ff822481e880cebfdfdd
LorenaCuervo <57373999+LorenaCuervo@users.noreply.github.com>
2019-11-27 00:37:49 +0100

    up

--- src/com/telefonica/jee/controllers/LogoutController.java
+++ src/com/telefonica/jee/controllers/LogoutController.java
@@ -8,17 +8,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class LoginController
+ * Servlet implementation class LogoutController
  */
-@WebServlet("/LoginController")
-public class LoginController extends HttpServlet {
+@WebServlet("/LogoutController")
+public class LogoutController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	private static final String HTTP_GET = "GET";
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public LoginController() {
+    public LogoutController() {
         super();
         // TODO Auto-generated constructor stub
     }
